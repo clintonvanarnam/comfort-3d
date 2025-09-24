f578290270a33b32874cda1ea89e5c9b0a7c1c6f
clintonvanarnam <info@clintonvanarnam.net>
2025-09-24 16:35:59 -0400

    sanity

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,6 @@ import dynamic from 'next/dynamic';
 // Dynamically import the heavy ThreeScene client component and disable SSR
 const ThreeScene = dynamic(() => import('@/components/ThreeScene'), {
   ssr: false,
-  // Small loading placeholder while the 3D scene loads
-  loading: () => <div style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>Loading…</div>
 });
 
 export default function Home() {
